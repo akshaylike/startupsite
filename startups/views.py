10c9159d84b6a7ed1589afcb799fc9cf38e14cd5
Akshay Balraj <akshaylike@gmail.com>
2014-05-27 10:31:53 +0530

    Second commit

--- startups/views.py
+++ startups/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render_to_response
 from startups.models import StartupDetails
 from django.template import RequestContext
-from django.http import HttpResponse
-from django.contrib.sessions.models import Session;
+from django.http import HttpResponse, HttpResponseRedirect
 
-previously_voted = 0
 def index(request):
   context = RequestContext(request)
   startups_list = StartupDetails.objects.all().order_by('-votes')
@@ -14,29 +12,53 @@ def index(request):
 
 def upvote(request):
   context = RequestContext(request)
-  startup_id = None
-  if request.method == 'GET':
-    startup_id = request.GET['s_id']
-  votes = 0
-  if startup_id:
-    startup = StartupDetails.objects.get(id=int(startup_id))
-    if startup:
-        votes = startup.votes + 1
-        startup.votes = votes
-        startup.save()
-  return HttpResponse(votes)
+  startup_id = request.GET['s_id']
+  vflag = 'Uvoted'.join(startup_id)
+  if vflag in request.session:
+    v = StartupDetails.objects.get(id=int(startup_id))
+    if vflag[:1] == 'D':
+      v.votes += 1
+      v.save()
+      return HttpResponse(v.votes)
+    else:
+      tempstr = (str(v.votes)) + ' already voted'
+      return HttpResponse(tempstr)
+  else:
+    if request.method == 'GET':
+      #startup_id = request.GET['s_id']
+      votes = 0
+      if startup_id:
+        startup = StartupDetails.objects.get(id=int(startup_id))
+        if startup:
+          votes = startup.votes + 1
+          startup.votes = votes
+          startup.save()
+          request.session[vflag] = True
+          return HttpResponse(votes)
+
 
 def downvote(request):
   context = RequestContext(request)
-  previously_voted = 0
-  startup_id = None
-  if request.method == 'GET':
-    startup_id = request.GET['s_id']
-  votes = 0
-  if startup_id:
-    startup = StartupDetails.objects.get(id=int(startup_id))
-    if startup:
-      votes = max(0, startup.votes - 1)
-      startup.votes = votes
-      startup.save()
-  return HttpResponse(votes)
+  startup_id = request.GET['s_id']
+  vflag = 'Dvoted'.join(startup_id)
+  if vflag in request.session:
+    v = StartupDetails.objects.get(id=int(startup_id))
+    if vflag[:1] == 'U':
+      v.votes -= 1
+      v.save()
+      return HttpResponse(v.votes)
+    else:
+      tempstr = (str(v.votes)) + ' already voted'
+      return HttpResponse(tempstr)
+  else:
+    if request.method == 'GET':
+    #startup_id = request.GET['s_id']
+      votes = 0
+      if startup_id:
+        startup = StartupDetails.objects.get(id=int(startup_id))
+        if startup:
+          votes = max(0, startup.votes - 1)
+          startup.votes = votes
+          startup.save()
+          request.session[vflag] = True
+          return HttpResponse(votes)
